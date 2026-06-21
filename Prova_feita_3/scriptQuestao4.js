2b70058ce93312df308e8a94a9bfd30ca597c566
igorDepieri2006 <igortrabalhos2006@gmail.com>
2026-06-21 12:52:42 -0300

    Add files via upload

---
+++ Prova_feita_3/scriptQuestao4.js
@@ -0,0 +1,33 @@
+function revelar(){
+    const imagem = document.querySelector('.card-img-top');
+    const spanNome = document.getElementById('Nome');
+    const spanData = document.getElementById('Data_Nas');
+    const spanAltura = document.getElementById('Altura'); 
+    const spanPosicao = document.querySelector('[id^="Posição"]'); 
+
+    if (imagem){
+        imagem.src='_vinicius_junior.png'; 
+        imagem.alt='Vinícius Júnior';
+    }
+    if (spanNome) {
+        spanNome.innerHTML = 'Vinícius José Paixão de Oliveira Júnior <span id="Rank" class="badge text-bg-success">9,5</span>';
+    }
+    if (spanData) {
+        spanData.innerText = "12/07/2000 (25 anos)";
+    }
+    if (spanAltura) {
+        spanAltura.innerText = "1,76 m";
+    }
+    if (spanPosicao) {
+        spanPosicao.innerText = "Ponta-esquerda / Atacante";
+    }
+    const placeholders = document.querySelectorAll('.placeholder');
+    placeholders.forEach(el => {
+        el.classList.remove('placeholder');
+        el.classList.add('card-text');
+    });
+    const glows = document.querySelectorAll('.placeholder-glow');
+    glows.forEach(el => {
+        el.classList.remove('placeholder-glow');
+    });
+}
